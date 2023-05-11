8b2ff41db22da1946b76a866397c23e111a863bf
Melnando <131868777+Melnando@users.noreply.github.com>
2023-05-11 23:57:55 +0200

    Update A-B brute force.py
    Là ça fonctionne

--- A-B brute force.py
+++ A-B brute force.py
@@ -1,6 +1,6 @@
+
 import sys
 from copy import deepcopy
-import itertools
 
 
 #%% Classes
@@ -10,30 +10,40 @@ class SubBoard:
         self.actions = [[i, j] for i in range(3) for j in range(3) if self.cells[i][j] == " "]
         self.winner = None
     
-    def result(self, action, joueur):
-        self.cells[action[0]][action[1]] = joueur
+    def result(self, action, player):
+        self.cells[action[0]][action[1]] = player
+        if self.TerminalTest(player):
+            self.winner = player
+            return
         self.actions.remove(action)
+  
 
         
 
-    def TerminalTest(self, player): 
+    def TerminalTest(self, player):
         # Vérification des lignes
-        if any(all(cell == player for cell in row) for row in self.cells):
-            self.winner = player
-            return True
+        for row in self.cells:
+            if row == [player] * 3:
+                self.winner = player
+                return True
 
         # Vérification des colonnes
-        if any(all(self.cells[row][col] == player for row in range(3)) for col in range(3)):
-            self.winner = player
-            return True
+        for col in range(3):
+            if [self.cells[row][col] for row in range(3)] == [player] * 3:
+                self.winner = player
+                return True
 
         # Vérification des diagonales
-        if all(self.cells[i][i] == player for i in range(3)) or all(self.cells[i][2 - i] == player for i in range(3)):
-            self.winner = player
+        if [self.cells[i][i] for i in range(3)] == [player] * 3 or [self.cells[i][2 - i] for i in range(3)] == [player] * 3:
+            if self.cells[1][1] != " ":
+                return True
+
+        # Vérification d'un match nul
+        if all(cell != " " for row in self.cells for cell in row):
+            self.winner = "tie"
             return True
 
         return False
-
 
 
 
@@ -45,36 +55,43 @@ class Board:
         
         
         
-        self.action = [[i,j] for i in range(3) for j in range(3) if self.sub_boards[i][j].winner is None]
+        
         
     
         self.current_player = "X"
         self.winner = None
+    
+    def action(self):
+        return [[i,j] for i in range(3) for j in range(3) if self.sub_boards[i][j].winner is None]
+        
     
      
     def terminalTest(self):
         current_player = self.current_player
 
         # Vérification des lignes et des colonnes
-        if any(all(self.sub_boards[i][j].winner == current_player for j in range(3)) for i in range(3)) \
-            or any(all(self.sub_boards[j][i].winner == current_player for j in range(3)) for i in range(3)):
-            self.winner = current_player
-            return True
-
+        for i in range(3):
+            if all(self.sub_boards[i][j].winner == current_player for j in range(3)):
+                self.winner = current_player
+                return True
+            if all(self.sub_boards[j][i].winner == current_player for j in range(3)):
+                self.winner = current_player
+                return True
+        
         # Vérification des diagonales
         if all(self.sub_boards[i][i].winner == current_player for i in range(3)) \
-            or all(self.sub_boards[i][2 - i].winner == current_player for i in range(3)):
+                or all(self.sub_boards[i][2 - i].winner == current_player for i in range(3)):
             self.winner = current_player
             return True
 
         # Vérification d'une égalité
-        if all(self.sub_boards[i][j].winner == 'tie' for i, j in self.action):
-            self.winner = 'tie'
+        if all(self.sub_boards[i][j].winner == "tie" for i in range(3) for j in range(3)):
+            self.winner = "tie"
             return True
 
         return False
 
-    
+     
 
     def result(self, in_Board, in_SubBoard):
         new_board = Board()
@@ -85,7 +102,8 @@ class Board:
         new_board.current_sub_board_ij = in_SubBoard
         new_board.sub_boards[in_Board[0]][in_Board[1]].result(in_SubBoard, new_board.current_player)
         if new_board.sub_boards[in_Board[0]][in_Board[1]].TerminalTest(new_board.current_player):
-            new_board.current_sub_board_ij = None
+            new_board.current_sub_board_ij = None       
+
             
         return new_board
 
@@ -95,9 +113,11 @@ class Board:
         if self.winner is None:
             return 0
         elif self.winner == self.current_player:
-            return 1
+            return 100
+        elif self.winner == "tie":
+            return 1  # Match nul
         else:
-            return -1
+            return -100
 
 
     # Affichage       
@@ -128,8 +148,6 @@ class Board:
 
 #%% Alpha Beta
 
-
-
 def Alpha_Beta_Search(board, depth):
     alpha = -sys.maxsize - 1
     beta = sys.maxsize
@@ -145,7 +163,7 @@ def Alpha_Beta_Search(board, depth):
                 best_action = [board.current_sub_board_ij, [action_i, action_j]]
         return best_action
     else:
-        for sub_i, sub_j in itertools.product(range(3), range(3)):
+        for [sub_i, sub_j] in board.action():
             if board.sub_boards[sub_i][sub_j].winner is None:
                 for action_i, action_j in board.sub_boards[sub_i][sub_j].actions:
                     new_board = board.result([sub_i, sub_j], [action_i, action_j])
@@ -157,26 +175,33 @@ def Alpha_Beta_Search(board, depth):
 
  
 def Max_Value(board, a, b, depth):
-    board.current_player = 'X'
+    board.current_player = 'O'
     if board.terminalTest() or depth == 0:
+        """
         print('end',board.utility())
+        board.print_board()
+        for subbords in board.sub_boards:
+            for subbbords in subbords:
+                print(subbbords.winner,end = ' ')
+                print(board.action())
+          """      
         return board.utility()
     v = -sys.maxsize - 1
     
-    if board.current_sub_board_ij is not None :
+    if board.current_sub_board_ij is not None and board.sub_boards[board.current_sub_board_ij[0]][board.current_sub_board_ij[1]].winner is None:
         for action_subBoard in board.sub_boards[board.current_sub_board_ij[0]][board.current_sub_board_ij[1]].actions:
-            new_board = deepcopy(board)
-            v = max(v, Min_Value(new_board.result(board.current_sub_board_ij, action_subBoard), a, b, depth - 1))
-            new_board.print_board()
+        # ...
+
+            v = max(v, Min_Value(board.result(board.current_sub_board_ij, action_subBoard), a, b, depth - 1))
+
             if v >= b:
                 return v
             a = max(a, v)
     else:
         
-        for action_board in board.action:       
+        for action_board in board.action():       
             for action_subBoard in board.sub_boards[action_board[0]][action_board[1]].actions:
-                new_board = deepcopy(board)
-                v = max(v, Min_Value(new_board.result(action_board, action_subBoard), a, b, depth - 1))
+                v = max(v, Min_Value(board.result(action_board, action_subBoard), a, b, depth - 1))
                 if v >= b:
                     return v
                 a = max(a, v)
@@ -184,25 +209,33 @@ def Max_Value(board, a, b, depth):
 
 def Min_Value(board, a, b, depth):
     
-    board.current_player = 'O'
+    board.current_player = 'X'
     if board.terminalTest() or depth == 0:
+        """
         print('end',board.utility())
+        board.print_board()
+        for subbords in board.sub_boards:
+            for subbbords in subbords:
+                print(subbbords.winner,end = ' ')
+           """  
+        
         return board.utility()
     v = sys.maxsize
-    if board.current_sub_board_ij is not None :
+    if board.current_sub_board_ij is not None and board.sub_boards[board.current_sub_board_ij[0]][board.current_sub_board_ij[1]].winner is None:
         for action_subBoard in board.sub_boards[board.current_sub_board_ij[0]][board.current_sub_board_ij[1]].actions:
-            
-            new_board = deepcopy(board)
-            v = min(v, Max_Value(new_board.result(board.current_sub_board_ij, action_subBoard), a, b, depth - 1))
+        # ...
+          
+
+            v = min(v, Max_Value(board.result(board.current_sub_board_ij, action_subBoard), a, b, depth - 1))
             if v <= a:
                 return v
             b = min(b, v)
     else:
-        for action_board in board.action:       
+        for action_board in board.action():       
             for action_subBoard in board.sub_boards[action_board[0]][action_board[1]].actions:
                
-                new_board = deepcopy(board)
-                v = min(v, Max_Value(new_board.result(action_board, action_subBoard), a, b, depth - 1))
+
+                v = min(v, Max_Value(board.result(action_board, action_subBoard), a, b, depth - 1))
                 if v <= a:
                     return v
                 b = min(b, v)
@@ -233,7 +266,7 @@ def play_ultimate_tic_tac_toe():
                     
                     
                     
-                    if in_board in board.action and in_sub_board in board.sub_boards[in_board[0]][in_board[1]].actions:
+                    if in_board in board.action() and in_sub_board in board.sub_boards[in_board[0]][in_board[1]].actions:
                         valid_input = True
                     else:
                         print("Coup invalide. Réessayez.")
@@ -244,9 +277,8 @@ def play_ultimate_tic_tac_toe():
             board.current_player = 'O'
         else:
             # Ordinateur (Alpha-Beta)
-            
             print("Tour de l'ordinateur (O)...")
-            best_action = Alpha_Beta_Search(board, 8)
+            best_action = Alpha_Beta_Search(board,  8)
             board = board.result(best_action[0], best_action[1])
             board.current_player = 'X'
 
@@ -262,4 +294,5 @@ def play_ultimate_tic_tac_toe():
 
 
 play_ultimate_tic_tac_toe()
+
 
